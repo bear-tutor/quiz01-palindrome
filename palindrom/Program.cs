ed0994529413494a94b0dc0e6ef0435376b8ecf1
Aphaphon <pui.aphaphon@gmail.com>
2018-10-07 14:49:32 +0700

    Create all projects.

---
+++ palindrom/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
